b82da9faf6c9e44888186cb19c4b1f4b2d2cd295
Jenson <jensonsantoso19@gmail.com>
2025-07-01 20:39:49 +0700

    added supabase depedency

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,7 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '@/types/supabase'; // Optional
+import { Database } from '@/types/supabase';
 
 export const dynamic = 'force-dynamic';
 
